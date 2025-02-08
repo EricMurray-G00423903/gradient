c204e7e71460c7ee12f5eac66dee6b4baea009ff
EricMurray-G00423903 <g00423903@atu.ie>
2025-02-08 23:12:32 +0000

    Changed frontend and added backend test function - working locally

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Null & Void is LIVE! 🔥</h1>
+      <p>Testing full CI/CD deployment to Hostinger (Frontend) & Firebase!</p>
     </div>
   );
 }
