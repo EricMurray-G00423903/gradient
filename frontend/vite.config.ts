ab0a82b5fc55c8433399e3826061dcae03054246
EricMurray-G00423903 <g00423903@atu.ie>
2025-02-13 23:29:31 +0000

    Installed Vite PWA - Created logos favicon.io - Used basic manifest file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,62 @@
-import { defineConfig} from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port : 3000,
-    }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Gradient",
+        short_name: "Gradient",
+        description: "An AI-powered portfolio and study planner.",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          }
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "html-cache",
+            },
+          },
+          {
+            urlPattern: ({ request }) =>
+              ["style", "script", "image"].includes(request.destination),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "asset-cache",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
 });
