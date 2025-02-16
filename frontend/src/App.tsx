0a8a1df40019dbd7fb7b30f1894ee63fe2359893
norakeavney <g00415845@atu.ie>
2025-02-16 00:05:44 +0000

    Added Login to Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import StudyPlanner from './Pages/StudyPlanner';
+import Login from './Pages/login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/study-planner" element={<StudyPlanner />} />
       </Routes>
     </Router>
--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -7,6 +7,7 @@ const Home= () => {
             <h1>Welcome to the Home Page</h1>
             <p>placeholder</p>
             <Link to="/study-planner">Go to Study Planner</Link>
+            <Link to="/login">Go to Logging In</Link>
         </div>
     );
 };
